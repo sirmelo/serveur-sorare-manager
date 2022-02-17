d2c764564e27357dccec10c975712e82e2608392
sirmelo <kevin.dovi@gmail.com>
2022-02-17 17:59:44 +0100

    test

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,7 @@ module.exports = {
   }],
   deploy: {
     production:{
+      user:'root',
       host:'164.92.205.219',
       ref:'origin/main',
       repo:'https://github.com/sirmelo/serveur-sorare-manager/',
