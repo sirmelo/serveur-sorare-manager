28202315cd3a361bdb79acfeaaa7701ab890f5df
sirmelo <kevin.dovi@gmail.com>
2022-02-18 13:29:28 +0100

    user

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,7 +9,7 @@ module.exports = {
       host:'164.92.205.219',
       ref:'origin/main',
       repo:'https://github.com/sirmelo/serveur-sorare-manager/',
-      path:'/home/serveur-sorare-manager',
+      path:'/home/sirmelo',
         'pre-deploy-local':'',
         'post-deploy':'npm install && pm2 reload ecosystem.config.js --env production',
         'pre-setup':''
