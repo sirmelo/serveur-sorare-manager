88559cdc7e740f8bd7395c9fe648c10742897f92
sirmelo <kevin.dovi@gmail.com>
2022-02-18 01:14:24 +0100

    vv

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
   deploy: {
     production:{
       user:'root',
-      host:'home-connect.net',
+      host:'164.92.205.219',
       ref:'origin/main',
       repo:'https://github.com/sirmelo/serveur-sorare-manager/',
       path:'/root/serveur-sorare-manager',
